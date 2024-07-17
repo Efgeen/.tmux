812ae55692bcb6bb60f415b2377a131d3cbc8b81
= <efgeen1998@gmail.com>
2024-07-17 09:54:39 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# assert
 if [ -f ~/.tmux.conf ]; then
     echo "[fail] : ~/.tmux.conf"
     exit 1
 fi
 
+# tmux
 pacman -S --needed --noconfirm tmux
 
+# ~/.tmux.conf
 ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
 
 echo "[success]"
+
