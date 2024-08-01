ae6540a40a2f19134131d4d866e773986a0fefe8
Efgeen <efgeen1998@gmail.com>
2024-08-01 15:59:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,46 +1,5 @@
 #!/bin/sh
 
-f=false
-
-while getopts ":f" opt; do
-    case $opt in
-	f)
-	    f=true
-	    ;;
-    esac
-done
-
-if [ "$f" = false ]; then
-    if [ -f ~/.tmux.conf ]; then
-	read -p "~/.tmux.conf exists, force? (y/n/<C-c>) : " yn
-	while true; do
-	    case "$yn" in
-	        [y])
-		    break
-		    ;;
-	        [n])
-            	    echo "[tmux] : fail (~/.tmux.conf)"
-            	    exit 1
-	            ;;
-	        *)
-		    read -p "invalid input, ~/.tmux.conf exists, force? (y/n/<C-c>) : " yn
-		    ;;
-	    esac
-	done
-    fi
-fi
-
-if ! pacman -S --needed --noconfirm tmux > /dev/null 2>&1; then
-    echo "[tmux] : fail (pacman)"
-    exit 2
-fi
-
+pacman -S --needed --noconfirm tmux
 rm -f ~/.tmux.conf
-
-if ! ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf; then
-    echo "[tmux] : fail (ln)"
-    exit 3
-fi
-
-echo "[tmux] : success"
-
+ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
