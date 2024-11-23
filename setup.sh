4efe7ec37cbe83e130bfb8e37ffb9296b40cbc4d
Efgeen <efgeen1998@gmail.com>
2024-11-23 23:50:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-pacman -S --noconfirm tmux
-rm -f ~/.tmux.conf
+echo -e "\033[0;32m[.nvim] : setup...\033[0m"
+
+# ~/.tmux.conf
+if [ -f ~/.tmux.conf ]; then
+    echo -e "\033[0;31m[.nvim] : fail (-f ~/.tmux.conf)\033[0m"
+    exit 1
+fi
+
+pacman -S --noconfirm --needed tmux
 ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
+
+echo -e "\033[0;32m[.nvim] : success\033[0m"
